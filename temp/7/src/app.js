f275753d3d57b587d1baa6360a9640fa1e5bb700
qiuzhifei <qiuzhifei521@gmail.com>
2018-07-06 17:58:10 +0800

    f

--- temp/7/src/app.js
+++ temp/7/src/app.js
@@ -16,72 +16,7 @@ const content = new Vue({
             });
     },
     data: {
-        items: [],
-        items1: [{
-                title: "title",
-                url: "https://source.unsplash.com/900x920"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/820x900"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/500x500"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/550x520"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/660x630"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/700x770"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/880x820"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/898x820"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/8998x920"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/898x960"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/999x888"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/1000x777"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/2000x777"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/2000x1000"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/3000x1000"
-            },
-            {
-                title: "title",
-                url: "https://source.unsplash.com/9000x9000"
-            },
-        ]
+        items: []
     }
 })
 
