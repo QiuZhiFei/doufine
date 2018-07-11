d4b89c518ba283c5662d4a88b077796179a93419
qiuzhifei <qiuzhifei521@gmail.com>
2018-07-11 16:44:42 +0800

    f

--- doufine/src/App.vue
+++ doufine/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserMoments msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserMoments from "./components/page/UserMoments.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UserMoments,
   }
 }
 </script>
---
+++ doufine/src/components/page/UserMoments.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="root">{{ msg }}</div>
+</template>
+
+
+<script>
+export default {
+    name: "UserMoments",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+.root {
+    background: yellow;
+    width: 100px;
+    height: 100px;
+}
+</style>
+
