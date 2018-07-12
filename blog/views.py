1a2f111be52b7c5a1d188305eb55189b0d27c28a
qiuzhifei <qiuzhifei521@gmail.com>
2018-07-12 15:57:13 +0800

    f

--- blog/views.py
+++ blog/views.py
@@ -74,5 +74,5 @@ def page_404():
 
 def handle_slack():
     print("request == ", request)
-    print("form == ", request.form.get["payload", ""])
+    print("form == ", request.form.get("payload", ""))
     return "123"
