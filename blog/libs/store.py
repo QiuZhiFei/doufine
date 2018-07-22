e659bbcdbd39ca1e5aa2b588138d1854cf0de351
qiuzhifei <qiuzhifei521@gmail.com>
2018-07-22 15:57:40 +0800

    f

--- blog/libs/store.py
+++ blog/libs/store.py
@@ -8,8 +8,8 @@ import libs.sql_helper as _db
 
 def addWB(item):
     item = {}
+    item["item_id"] = "4263234627933203"
     item["created_at"] = "07-18"
-    item["id"] = "4263234627933203"
     item["original_pic"] = "http://wx2.sinaimg.cn/large/badcfc6dly1fteaufdl16j20qo0zkh0s.jpg"
     item["scheme"] = "https://m.weibo.cn/status/Gqx1kxhMT?mblogid=Gqx1kxhMT&luicode=10000011&lfid=1076033135044717"
     item["text"] = "伏树哀鸣，为爱恨所苦者，蝉。"
@@ -20,7 +20,9 @@ def addWB(item):
     `original_pic`, `scheme`, `text`, \
     `title`) \
     VALUES \
-    (NULL, '1', '2', '3', '4', '5', '6')"
+    (NULL, '%s', '%s', '%s', '%s', '%s', '%s')" % \
+    (item["item_id"], item["created_at"], item["original_pic"], \
+    item["scheme"], item["text"], item["title"])
 
     _db.execute(cmd)
     
