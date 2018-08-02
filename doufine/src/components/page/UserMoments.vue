224b1ef6ede07f04ee0efc58321d514531d4f94f
qiuzhifei <qiuzhifei521@gmail.com>
2018-08-02 17:01:07 +0800

    f

--- doufine/src/components/page/UserMoments.vue
+++ doufine/src/components/page/UserMoments.vue
@@ -29,18 +29,18 @@
 
 
 <script>
-import Vue from "vue"
+import Vue from "vue";
 
-import infiniteScroll from "vue-infinite-scroll"
-Vue.use(infiniteScroll)
+import infiniteScroll from "vue-infinite-scroll";
+Vue.use(infiniteScroll);
 
-import LoadMore from "./../views/LoadMore.vue"
+import LoadMore from "./../views/LoadMore.vue";
 
 export default {
   name: "UserMoments",
   components: {
     infiniteScroll,
-    LoadMore,
+    LoadMore
   },
   props: {
     items: {
@@ -79,9 +79,10 @@ export default {
       }
 
       const _this = this;
-      Vue.set(content, 'isLoading', true);
+      Vue.set(content, "isLoading", true);
 
-      const url = "http://140.143.239.212:5000/user/moments?p=" + page;
+      const url = "http://api.doufine.com/user/moments?p=" + page;
+      //   const url = "http://140.143.239.212:5000/user/moments?p=" + page;
       // const url = 'http://192.168.3.21:5000/user/moments?p=' + page;
 
       fetch(url)
@@ -104,114 +105,120 @@ export default {
 
 <style>
 .content {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 
 .content-container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .content-container .item {
-    width: 290px;
-    height: 390px;
-    margin: 12px;
-    /* overflow: hidden; */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border-radius: 3px;
-    -webkit-transition: 200ms ease-in-out;
-    -o-transition: 200ms ease-in-out;
-    transition: 200ms ease-in-out;
-    -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.7);
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.7);
-    text-decoration: none;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
+  width: 290px;
+  height: 390px;
+  margin: 12px;
+  /* overflow: hidden; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 3px;
+  -webkit-transition: 200ms ease-in-out;
+  -o-transition: 200ms ease-in-out;
+  transition: 200ms ease-in-out;
+  -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.7);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.7);
+  text-decoration: none;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 }
 
 .content-container .item:hover {
-    -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.7);
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.7);
+  -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.7);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.7);
 }
 
 .content-container .item .dete-container {
-    width: 100%;
-    height: 22px;
-    margin: 22px 0;
-    color: #fff;
-    text-align: center;
-    text-shadow: 0 0 3px rgba(0, 0, 0, 0.9);
+  width: 100%;
+  height: 22px;
+  margin: 22px 0;
+  color: #fff;
+  text-align: center;
+  text-shadow: 0 0 3px rgba(0, 0, 0, 0.9);
 }
 
 .content-container .item .des-container {
-    width: 100%;
-    background: rgba(0, 0, 0, 0.8);
-    height: 96px;
-    margin-top: 81px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  height: 96px;
+  margin-top: 81px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
 .content-container .item .des-container div {
-    padding: 20px;
+  padding: 20px;
 }
 
 .content-container .item .logo-container {
-    position: relative;
-    margin-top: 60px;
-    width: 80px;
-    height: 80px;
-    border-radius: 40px;
-    /* width: 80px;
+  position: relative;
+  margin-top: 60px;
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  /* width: 80px;
     height: 80px;
     position: relative;
     border-radius: 100px;
     margin: 85px auto; */
-    /*  */
-    background: #2b5876;
-    /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #4e4376, #2b5876);
-    /* Chrome 10-25, Safari 5.1-6 */
-    background: -webkit-gradient(linear, left top, right top, from(#4e4376), to(#2b5876));
-    background: -webkit-linear-gradient(left, #4e4376, #2b5876);
-    background: -o-linear-gradient(left, #4e4376, #2b5876);
-    background: linear-gradient(to right, #4e4376, #2b5876);
-    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    -webkit-box-shadow: 0 0 20px darkblue;
-    box-shadow: 0 0 20px darkblue;
-    /* background: #2b5876;
+  /*  */
+  background: #2b5876;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #4e4376, #2b5876);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#4e4376),
+    to(#2b5876)
+  );
+  background: -webkit-linear-gradient(left, #4e4376, #2b5876);
+  background: -o-linear-gradient(left, #4e4376, #2b5876);
+  background: linear-gradient(to right, #4e4376, #2b5876);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  -webkit-box-shadow: 0 0 20px darkblue;
+  box-shadow: 0 0 20px darkblue;
+  /* background: #2b5876;
     box-shadow: 0 0 20px #2b5876; */
-    /* background: #f857a6; */
-    /* box-shadow: 0 0 20px #f857a6; */
-    display: flex;
-    align-items: center;
-    text-align: center;
-    justify-content: center;
+  /* background: #f857a6; */
+  /* box-shadow: 0 0 20px #f857a6; */
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
 }
 
 .content-container .item .logo-container h4 {
-    color: #fff;
-    font-size: 40px;
-    text-align: center;
-    font-weight: 100;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* top: 16px; */
+  color: #fff;
+  font-size: 40px;
+  text-align: center;
+  font-weight: 100;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* top: 16px; */
 }
 
 .load-more {
-    width: 100%;
-    height: 50px;
+  width: 100%;
+  height: 50px;
 }
 </style>
 
